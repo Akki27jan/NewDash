653deb31973281a2e8efdef0d01b13793cb54102
akki27jan <dubeyakshat2007@gmail.com>
2026-06-18 18:49:26 +0530

    fix vercel cors

--- backend_api/main.py
+++ backend_api/main.py
@@ -25,7 +25,8 @@ app.add_middleware(
         "http://localhost:3000", 
         "http://127.0.0.1:3000",
         settings.FRONTEND_URL
-    ], 
+    ],
+    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
